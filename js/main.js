df263d5df28e19297964fa70cea5da7c2e1fd828
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-07-04 02:16:42 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ const burgerCloseBtn = body.querySelector('.burger__close')
 
 const burgerItems = document.querySelector('.burger__items')
 
+const burgerContent = document.querySelector('.burger__content')
+
+burgerContent.querySelectorAll('.burger__item').forEach(el => {
+    el.addEventListener('click', e => {
+        burgerItems.classList.add('burger__items-passive')
+        burgerItems.classList.remove('burger__items-active')
+    })
+})
+
 burgerCloseBtn.addEventListener('click', e => {
     burgerItems.classList.add('burger__items-passive')
     burgerItems.classList.remove('burger__items-active')
